fd257fb037930a91fcc3487494647d55d86d175f
mick84 <demenkoff1984@yahoo.com>
2022-10-22 21:51:13 +0300

    theme-night added

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,105 @@
 "use strict";
 {
+    const trees = [
+        {
+            name: "poplar",
+            height: 7,
+            width: 5,
+            scheme: [
+                ["", "", "l", "", ""],
+                ["", "l", "l", "l", ""],
+                ["l", "l", "l", "l", "l"],
+                ["", "l", "w", "l", ""],
+                ["", "", "w", "", ""],
+                ["", "", "w", "", ""],
+                ["", "", "w", "", ""],
+            ],
+        },
+        {
+            name: "spruce",
+            height: 7,
+            width: 7,
+            scheme: [
+                ["", "", "", "l", "", "", ""],
+                ["", "", "l", "l", "l", "", ""],
+                ["", "l", "l", "l", "l", "l", ""],
+                ["l", "l", "l", "l", "l", "l", "l"],
+                ["", "", "", "w", "", "", ""],
+                ["", "", "", "w", "", "", ""],
+                ["", "", "", "w", "", "", ""],
+            ],
+        },
+    ];
     const introPage = document.getElementById("intro-page");
+    const startBtn = introPage.querySelector("#start");
     const gamePage = document.getElementById("game-page");
     const board = gamePage.querySelector("#board");
-    const startBtn = introPage.querySelector("#start");
+    const toolCtrl = gamePage.querySelector("#tools");
+    const inventoryCtrl = gamePage.querySelector("#inventory");
     const SOIL_HEIGHT = 2;
-    const STONE_HEIGHT = 3;
+    const STONE_HEIGHT = 2;
+    const GRASS_HEIGHT = 1;
     class App {
         constructor() {
-            this.tools = ["axe", "pickaxe", "shovel", "scythe"];
-            this.currentTool = this.tools[0];
-            this.inventory = { stones: 0, soil: 0, wood: 0, grass: 0 };
             this.maxInventory = 12;
-            this.start();
+            this.direction = "right";
+            this.coords = {
+                row: 16,
+                col: 2,
+            };
             const numOfRows = getComputedStyle(board)
                 .getPropertyValue("grid-template-rows")
                 .split(" ").length;
-            console.log(numOfRows);
             this.fillGrid(STONE_HEIGHT, numOfRows, "stones");
             this.fillGrid(SOIL_HEIGHT, numOfRows - STONE_HEIGHT, "soil");
+            this.fillGrid(GRASS_HEIGHT, numOfRows - STONE_HEIGHT - SOIL_HEIGHT, "grass");
+            startBtn.addEventListener("click", () => this.start());
+            board.addEventListener("click", function (e) {
+                //2 modes: take item to inventory, and put item from inventory.
+                //need addeventlistener instead!
+                const el = e.target;
+                const item = el.dataset?.item ?? "";
+                if (item in App.inventory && el.dataset.tool === App.currentTool) {
+                    //take to inventory:
+                    App.inventory[item]++;
+                    //update number on the screen:
+                    //find element to update by data-item
+                    const toUpdate = [...inventoryCtrl.children].find((el) => el.firstElementChild.getAttribute("data-item") === item);
+                    toUpdate.querySelector(".quantity").textContent = `${App.inventory[item]}`;
+                    el.remove();
+                }
+            });
+            toolCtrl.addEventListener("click", function (e) {
+                const tool = e.target;
+                if (App.tools.includes(tool.id)) {
+                    App.currentTool = tool.id;
+                    const activeTool = toolCtrl.querySelector(".active");
+                    const newToolIndex = App.tools.indexOf(App.currentTool);
+                    activeTool?.classList.remove("active");
+                    toolCtrl.children[newToolIndex].classList.add("active");
+                }
+            });
         }
         resetState() {
-            this.currentTool = this.tools[0];
-            for (const key in this.inventory) {
-                this.inventory[key] = 0;
+            App.currentTool = "";
+            for (const key in App.inventory) {
+                App.inventory[key] = 0;
             }
         }
         start() {
             console.log("start");
             introPage.classList.toggle("hidden");
             gamePage.classList.toggle("hidden");
+            const cells = [...board.children];
+            const cell = cells.find((el) => {
+                const crds = el.style.gridArea.split(" ").filter((el) => !isNaN(+el));
+                return +crds[0] === this.coords.row && +crds[1] === this.coords.col;
+            });
+            console.log(cell);
+        }
+        plantTree(row, col, treeName) {
+            //check if cell[row][col] is grass
+            //check if there are free rectangle with height 7 and width 5 for tree or 7 for spruce:
         }
         fillGrid(depth, bottom, material) {
             const boardWidth = getComputedStyle(board)
@@ -43,8 +113,8 @@
                 for (let j = 1; j <= boardWidth; j++) {
                     //create element:
                     const block = document.createElement("div");
-                    block.classList.add("block", material);
-                    let tool;
+                    block.classList.add("block");
+                    let tool = "scythe";
                     switch (material) {
                         case "soil":
                             tool = "shovel";
@@ -59,20 +129,16 @@
                             tool = "scythe";
                             break;
                     }
+                    block.setAttribute("data-item", material);
                     block.setAttribute("data-tool", tool);
-                    block.style.gridArea = `${i} /${j} / span 1/ span 1`;
+                    block.style.gridArea = `${i} / ${j}`;
                     board.appendChild(block);
                 }
             }
         }
     }
-    const app = new App();
-    startBtn.onclick = () => app.start();
-    board.onclick = function (e) {
-        const el = e.target;
-        const classes = [...el.classList];
-        if (classes.some((cls) => cls in app.inventory)) {
-            console.log("ok");
-        }
-    };
+    App.tools = ["axe", "pickaxe", "shovel", "scythe"];
+    App.currentTool = "";
+    App.inventory = { stones: 0, soil: 0, wood: 0, grass: 0 };
+    new App();
 }
